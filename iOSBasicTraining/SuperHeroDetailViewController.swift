96d99df0efd73c709ee4b747eda73863cdf89a6d
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-06-24 15:38:31 +0200

    Complete super hero detail view controller

--- iOSBasicTraining/SuperHeroDetailViewController.swift
+++ iOSBasicTraining/SuperHeroDetailViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SDWebImage
 
 class SuperHeroDetailViewController: UIViewController {
 
@@ -16,7 +17,25 @@ class SuperHeroDetailViewController: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var avengersBadgeImageView: UIImageView!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let superHero = SuperHero(id: "3",
+                                  name: "The Flash",
+                                  description: "The fastest men alive",
+                                  image: NSURL(string: "https://i0.wp.com/freakelitex.com/wp-content/uploads/2015/12/barry-allen-in-the-flash-season-2-tv-poster.jpg?resize=1024%2C640"),
+                                  comics: [Comic]())
+        showSuperHero(superHero)
+    }
+
     @IBAction func didTapCaptureButton() {
-        
+
+    }
+
+    private func showSuperHero(superHero: SuperHero) {
+        title = superHero.name.uppercaseString
+        nameLabel.text = superHero.name
+        descriptionLabel.text = superHero.description
+        avengersBadgeImageView.image = UIImage(named: "ic_avengers_badge")
+        photoImageView.sd_setImageWithURL(superHero.image)
     }
 }
