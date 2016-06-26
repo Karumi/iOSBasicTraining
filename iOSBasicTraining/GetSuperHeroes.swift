4821fee70325507d6535e4c39689d94fe83be0d2
pedrovgs <pedrovicente.gomez@gmail.com>
2016-06-26 13:15:56 +0200

    Create two use cases

---
+++ iOSBasicTraining/GetSuperHeroes.swift
@@ -0,0 +1,25 @@
+//
+//  GetSuperHeroes.swift
+//  iOSBasicTraining
+//
+//  Created by Pedro Vicente on 26/6/16.
+//  Copyright © 2016 GoKarumi. All rights reserved.
+//
+
+import Foundation
+import Result
+
+class GetSuperHeroes {
+
+    private let superHeroesDetector: SuperHeroesDetector
+    
+    init(superHeroesDetector: SuperHeroesDetector) {
+        self.superHeroesDetector = superHeroesDetector
+    }
+    
+    func getAll(completion: (Result<[SuperHero], SuperHeroesDetectorError>) -> Void) {
+        superHeroesDetector.getSuperHeroes { result in
+            completion(result)
+        }
+    }
+}
